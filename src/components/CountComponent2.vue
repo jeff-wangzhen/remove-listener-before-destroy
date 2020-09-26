51e8b45fa4fe0d205c311962e4348f585083f870
wangzhen <kill370354@qq.com>
2020-09-26 18:22:33 +0800

    chore: 注释增删

--- src/components/CountComponent2.vue
+++ src/components/CountComponent2.vue
@@ -13,10 +13,6 @@ export default {
   },
   beforeDestroy() {
     console.log("2 beforeDestroy");
-    // 移除所有监听事件，当本组件被引用多次时，所有引用的监听全部移除
-    // this.notify.$off("notify");
-    // 移除所有监听事件，当本组件被引用多次时，所有引用的监听全部移除
-    // this.notify.$off("notify", this.listen);
   },
   methods: {
     listen() {
--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-
+// 创建第三方组件，专门用于事件派发与监听
 const notify = new Vue();
 Vue.prototype.notify = notify;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
